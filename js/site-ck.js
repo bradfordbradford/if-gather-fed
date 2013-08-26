23b1a1c2ea80c3f22a567573587d743ec0e2ff25
bradfordbradford <bradford@wearetelegraph.com>
2013-08-26 07:57:29 -0500

    Monday Morning

--- js/site-ck.js
+++ js/site-ck.js
@@ -1,2 +1,4 @@
 // If/Gather Scripts
-$(document).foundation();
+// -----------------
+// Hover Image Overlays
+$(".image-list li").hover(function(){$(this).addClass("active").animate({right:parseInt(nw)+ -nw+"px"},200)},function(){$(this).removeClass("active").animate({right:parseInt(-nw)+60+"px"},200)});$(document).foundation();
--- js/site.js
+++ js/site.js
@@ -1,6 +1,13 @@
 
 
 // If/Gather Scripts
+// -----------------
 
 
+// Hover Image Overlays
+$('.image-list li').hover(function(){$(this).addClass("active").animate({right:(parseInt(nw) + -nw) + 'px'}, 200)},
+function(){$(this).removeClass("active").animate({right:(parseInt(-nw) + 60) + 'px'}, 200)});
+
+
+// Start Foundation
 $(document).foundation();
