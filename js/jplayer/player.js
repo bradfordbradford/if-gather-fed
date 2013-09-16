e518fd05a10b8dbb2bea33ae4e724791bb84f082
bradfordbradford <bradford@wearetelegraph.com>
2013-09-16 18:36:23 -0500

    project finish according to PSDs -launch ready, no revisions

---
+++ js/jplayer/player.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+    $("ul.image-list li a.track").click(function(event){
+        event.preventDefault();
+    });
+
+    // Trigger jPlayer for Unleash Page --------------
+    // Local copy of jQuery selectors, for performance.
+    var my_jPlayer = $("#jquery_jplayer"),
+        my_trackName = $("#player-wrap .track-name"),
+        my_playState = $("#player-wrap .play-state"),
+        my_extraPlayInfo = $("#player-wrap .extra-play-info");
+
+    // Some options
+    var opt_play_first = false, // If true, will attempt to auto-play the default track on page loads. No effect on mobile devices, like iOS.
+    opt_auto_play = true, // If true, when a track is selected, it will auto-play.
+    opt_text_playing = "Now playing", // Text when playing
+    opt_text_selected = "Track selected"; // Text when not playing
+
+    // A flag to capture the first track
+    var first_track = true;
+
+    // Change the time format
+    // $.jPlayer.timeFormat.padMin = false;
+    // $.jPlayer.timeFormat.padSec = false;
+    // $.jPlayer.timeFormat.sepMin = " min ";
+    // $.jPlayer.timeFormat.sepSec = " sec";
+
+    // Initialize the play state text
+    // my_playState.text(opt_text_selected);
+
+    // Instance jPlayer
+    my_jPlayer.jPlayer({
+        ready: function () {
+            $("#player-wrap .track-default").click();
+        },
+        timeupdate: function(event) {
+            my_extraPlayInfo.text(parseInt(event.jPlayer.status.currentPercentAbsolute, 10) + "%");
+        },
+        play: function(event) {
+            my_playState.text(opt_text_playing);
+        },
+        pause: function(event) {
+            my_playState.text(opt_text_selected);
+        },
+        ended: function(event) {
+            my_playState.text(opt_text_selected);
+        },
+        swfPath: "js",
+        cssSelectorAncestor: "#player-wrap",
+        supplied: "mp3, m4a",
+        wmode: "window"
+    });
+
+    // Create click handlers for the different tracks
+    $("#player-wrap .track").hover(function(e) {
+        my_trackName.text($(this).text());
+        my_jPlayer.jPlayer("setMedia", {
+            mp3: $(this).attr("href")
+        });
+        my_jPlayer.jPlayer("play");
+        first_track = false;
+        $(this).blur();
+        return false;
+    });
+
+    $("#player-wrap .track").mouseout(function(e) {
+        my_trackName.text($(this).text());
+        my_jPlayer.jPlayer("setMedia", {
+            mp3: $(this).attr("href")
+        });
+        my_jPlayer.jPlayer("stop");
+        first_track = false;
+        $(this).blur();
+        return false;
+    });
+
+    $("#player-wrap .track").mouseout(function(e) {
+        my_trackName.text($(this).text());
+        my_jPlayer.jPlayer("setMedia", {
+            m4a: $(this).attr("href")
+        });
+        my_jPlayer.jPlayer("stop");
+        first_track = false;
+        $(this).blur();
+        return false;
+    });
+
+
+});
--- js/site.js
+++ js/site.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
     $("body").css("display", "none");
     $("body").fadeIn(700);
 
+
     $("nav ul li a").click(function(event){
         event.preventDefault();
         linkLocation = this.href;
@@ -22,76 +23,80 @@ $(document).ready(function(){
         window.location = linkLocation;
     }
 
-    // Trigger jPlayer for Unleash Page --------------
-    // Local copy of jQuery selectors, for performance.
-    var my_jPlayer = $("#jquery_jplayer"),
-        my_trackName = $("#player-wrap .track-name"),
-        my_playState = $("#player-wrap .play-state"),
-        my_extraPlayInfo = $("#player-wrap .extra-play-info");
+    // $("ul.image-list li a.track").click(function(event){
+    //     event.preventDefault();
+    // });
 
-    // Some options
-    var opt_play_first = false, // If true, will attempt to auto-play the default track on page loads. No effect on mobile devices, like iOS.
-    opt_auto_play = true, // If true, when a track is selected, it will auto-play.
-    opt_text_playing = "Now playing", // Text when playing
-    opt_text_selected = "Track selected"; // Text when not playing
+    // // Trigger jPlayer for Unleash Page --------------
+    // // Local copy of jQuery selectors, for performance.
+    // var my_jPlayer = $("#jquery_jplayer"),
+    //     my_trackName = $("#player-wrap .track-name"),
+    //     my_playState = $("#player-wrap .play-state"),
+    //     my_extraPlayInfo = $("#player-wrap .extra-play-info");
 
-    // A flag to capture the first track
-    var first_track = true;
+    // // Some options
+    // var opt_play_first = false, // If true, will attempt to auto-play the default track on page loads. No effect on mobile devices, like iOS.
+    // opt_auto_play = true, // If true, when a track is selected, it will auto-play.
+    // opt_text_playing = "Now playing", // Text when playing
+    // opt_text_selected = "Track selected"; // Text when not playing
 
-    // Change the time format
-    // $.jPlayer.timeFormat.padMin = false;
-    // $.jPlayer.timeFormat.padSec = false;
-    // $.jPlayer.timeFormat.sepMin = " min ";
-    // $.jPlayer.timeFormat.sepSec = " sec";
+    // // A flag to capture the first track
+    // var first_track = true;
 
-    // Initialize the play state text
-    // my_playState.text(opt_text_selected);
+    // // Change the time format
+    // // $.jPlayer.timeFormat.padMin = false;
+    // // $.jPlayer.timeFormat.padSec = false;
+    // // $.jPlayer.timeFormat.sepMin = " min ";
+    // // $.jPlayer.timeFormat.sepSec = " sec";
 
-    // Instance jPlayer
-    my_jPlayer.jPlayer({
-        ready: function () {
-            $("#player-wrap .track-default").click();
-        },
-        timeupdate: function(event) {
-            my_extraPlayInfo.text(parseInt(event.jPlayer.status.currentPercentAbsolute, 10) + "%");
-        },
-        play: function(event) {
-            my_playState.text(opt_text_playing);
-        },
-        pause: function(event) {
-            my_playState.text(opt_text_selected);
-        },
-        ended: function(event) {
-            my_playState.text(opt_text_selected);
-        },
-        swfPath: "js",
-        cssSelectorAncestor: "#player-wrap",
-        supplied: "mp3",
-        wmode: "window"
-    });
+    // // Initialize the play state text
+    // // my_playState.text(opt_text_selected);
 
-    // Create click handlers for the different tracks
-    $("#player-wrap .track").hover(function(e) {
-        my_trackName.text($(this).text());
-        my_jPlayer.jPlayer("setMedia", {
-            mp3: $(this).attr("href")
-        });
-        my_jPlayer.jPlayer("play");
-        first_track = false;
-        $(this).blur();
-        return false;
-    });
+    // // Instance jPlayer
+    // my_jPlayer.jPlayer({
+    //     ready: function () {
+    //         $("#player-wrap .track-default").click();
+    //     },
+    //     timeupdate: function(event) {
+    //         my_extraPlayInfo.text(parseInt(event.jPlayer.status.currentPercentAbsolute, 10) + "%");
+    //     },
+    //     play: function(event) {
+    //         my_playState.text(opt_text_playing);
+    //     },
+    //     pause: function(event) {
+    //         my_playState.text(opt_text_selected);
+    //     },
+    //     ended: function(event) {
+    //         my_playState.text(opt_text_selected);
+    //     },
+    //     swfPath: "js",
+    //     cssSelectorAncestor: "#player-wrap",
+    //     supplied: "mp3",
+    //     wmode: "window"
+    // });
 
-    $("#player-wrap .track").mouseout(function(e) {
-        my_trackName.text($(this).text());
-        my_jPlayer.jPlayer("setMedia", {
-            mp3: $(this).attr("href")
-        });
-        my_jPlayer.jPlayer("stop");
-        first_track = false;
-        $(this).blur();
-        return false;
-    });
+    // // Create click handlers for the different tracks
+    // $("#player-wrap .track").hover(function(e) {
+    //     my_trackName.text($(this).text());
+    //     my_jPlayer.jPlayer("setMedia", {
+    //         mp3: $(this).attr("href")
+    //     });
+    //     my_jPlayer.jPlayer("play");
+    //     first_track = false;
+    //     $(this).blur();
+    //     return false;
+    // });
+
+    // $("#player-wrap .track").mouseout(function(e) {
+    //     my_trackName.text($(this).text());
+    //     my_jPlayer.jPlayer("setMedia", {
+    //         mp3: $(this).attr("href")
+    //     });
+    //     my_jPlayer.jPlayer("stop");
+    //     first_track = false;
+    //     $(this).blur();
+    //     return false;
+    // });
 
 });
 
@@ -125,5 +130,7 @@ var msnry = new Masonry( container, {
 * http://srobbin.com/jquery-plugins/backstretch/
 * Copyright (c) 2012 Scott Robbin; Licensed MIT */
 (function(e,t,n){"use strict";e.fn.backstretch=function(r,s){return(r===n||r.length===0)&&e.error("No images were supplied for Backstretch"),e(t).scrollTop()===0&&t.scrollTo(0,0),this.each(function(){var t=e(this),n=t.data("backstretch");n&&(s=e.extend(n.options,s),n.destroy(!0)),n=new i(this,r,s),t.data("backstretch",n)})},e.backstretch=function(t,n){return e("body").backstretch(t,n).data("backstretch")},e.expr[":"].backstretch=function(t){return e(t).data("backstretch")!==n},e.fn.backstretch.defaults={centeredX:!0,centeredY:!0,duration:5e3,fade:0};var r={wrap:{left:0,top:0,overflow:"hidden",margin:0,padding:0,height:"100%",width:"100%",zIndex:-999999},img:{position:"absolute",display:"none",margin:0,padding:0,border:"none",width:"auto",height:"auto",maxWidth:"none",zIndex:-999999}},i=function(n,i,o){this.options=e.extend({},e.fn.backstretch.defaults,o||{}),this.images=e.isArray(i)?i:[i],e.each(this.images,function(){e("<img />")[0].src=this}),this.isBody=n===document.body,this.$container=e(n),this.$wrap=e('<div class="backstretch"></div>').css(r.wrap).appendTo(this.$container),this.$root=this.isBody?s?e(t):e(document):this.$container;if(!this.isBody){var u=this.$container.css("position"),a=this.$container.css("zIndex");this.$container.css({position:u==="static"?"relative":u,zIndex:a==="auto"?0:a,background:"none"}),this.$wrap.css({zIndex:-999998})}this.$wrap.css({position:this.isBody&&s?"fixed":"absolute"}),this.index=0,this.show(this.index),e(t).on("resize.backstretch",e.proxy(this.resize,this)).on("orientationchange.backstretch",e.proxy(function(){this.isBody&&t.pageYOffset===0&&(t.scrollTo(0,1),this.resize())},this))};i.prototype={resize:function(){try{var e={left:0,top:0},n=this.isBody?this.$root.width():this.$root.innerWidth(),r=n,i=this.isBody?t.innerHeight?t.innerHeight:this.$root.height():this.$root.innerHeight(),s=r/this.$img.data("ratio"),o;s>=i?(o=(s-i)/2,this.options.centeredY&&(e.top="-"+o+"px")):(s=i,r=s*this.$img.data("ratio"),o=(r-n)/2,this.options.centeredX&&(e.left="-"+o+"px")),this.$wrap.css({width:n,height:i}).find("img:not(.deleteable)").css({width:r,height:s}).css(e)}catch(u){}return this},show:function(t){if(Math.abs(t)>this.images.length-1)return;this.index=t;var n=this,i=n.$wrap.find("img").addClass("deleteable"),s=e.Event("backstretch.show",{relatedTarget:n.$container[0]});return clearInterval(n.interval),n.$img=e("<img />").css(r.img).bind("load",function(t){var r=this.width||e(t.target).width(),o=this.height||e(t.target).height();e(this).data("ratio",r/o),e(this).fadeIn(n.options.speed||n.options.fade,function(){i.remove(),n.paused||n.cycle(),n.$container.trigger(s,n)}),n.resize()}).appendTo(n.$wrap),n.$img.attr("src",n.images[t]),n},next:function(){return this.show(this.index<this.images.length-1?this.index+1:0)},prev:function(){return this.show(this.index===0?this.images.length-1:this.index-1)},pause:function(){return this.paused=!0,this},resume:function(){return this.paused=!1,this.next(),this},cycle:function(){return this.images.length>1&&(clearInterval(this.interval),this.interval=setInterval(e.proxy(function(){this.paused||this.next()},this),this.options.duration)),this},destroy:function(n){e(t).off("resize.backstretch orientationchange.backstretch"),clearInterval(this.interval),n||this.$wrap.remove(),this.$container.removeData("backstretch")}};var s=function(){var e=navigator.userAgent,n=navigator.platform,r=e.match(/AppleWebKit\/([0-9]+)/),i=!!r&&r[1],s=e.match(/Fennec\/([0-9]+)/),o=!!s&&s[1],u=e.match(/Opera Mobi\/([0-9]+)/),a=!!u&&u[1],f=e.match(/MSIE ([0-9]+)/),l=!!f&&f[1];return!((n.indexOf("iPhone")>-1||n.indexOf("iPad")>-1||n.indexOf("iPod")>-1)&&i&&i<534||t.operamini&&{}.toString.call(t.operamini)==="[object OperaMini]"||u&&a<7458||e.indexOf("Android")>-1&&i&&i<533||o&&o<6||"palmGetResource"in t&&i&&i<534||e.indexOf("MeeGo")>-1&&e.indexOf("NokiaBrowser/8.5.0")>-1||l&&l<=6)}()})(jQuery,window);
+
+
 
 
